b60cbe6432324a666be7ba3f5658b591648718dc
Md Tabish <mdtabish@Mds-MacBook-Air.local>
2024-07-09 19:59:55 +0530

    added model viewcontroller

---
+++ HydrationTracker/Views/TableViewCell/WaterLogTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WaterLogTableViewCell.swift
+//  HydrationTracker
+//
+//  Created by Sanoj Kumar on 08/07/24.
+//
+
+import UIKit
+
+class WaterLogTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
